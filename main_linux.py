c86b831d3ea3b5022808c3125e1081c9c72324ea
BPPL0806 <bartekpolmusic@gmail.com>
2023-03-21 15:55:53 +0100

    Major update

--- main_linux.py
+++ main_linux.py
@@ -3,45 +3,78 @@
 import mysql.connector
 from os import system
 
-host_input = input("Enter database address: ")
-username_input = input(f"Enter username for {host_input}: ")
-passw_input = input(f"Enter password for {username_input}: ")
+close = exit
 
-try:
-    database = mysql.connector.connect(
-    host = host_input,
-    user = username_input,
-    password = passw_input
-    )
+def exit():
+    print("\nExiting...\n")
+    input("Press enter to exit...")
+    close(0)
+
+def clear():
+    system("clear")
+
+def help():
+    print("\nhelp   Display this message\nclear   Clear the screen\nexit   Quit commandline\n")
+
+commands = {
+    "exit" : exit,
+    "clear" : clear,
+    "help" : help,
+    #"save" : save
+}
+
+v = "2.0.0"
+
+print(f"MySQL Commandline [Version {v}]\n(c) MIT License\n\n(Press CTRL+C to exit.)\n")
+
+while True:
+    try:
+        host_input = input("Enter database address: ")
+        username_input = input(f"Enter username for {host_input}: ")
+        passw_input = input(f"Enter password for {username_input}: ")
+        
+        database = mysql.connector.connect(
+        host = host_input,
+        user = username_input,
+        password = passw_input
+        )
+        break
     
-except:
-    print("\n[Invalid credentials.]")
-    input("Press Enter to exit...")
-    exit()
+    except KeyboardInterrupt:
+        exit()
+
+    except mysql.connector.errors.ProgrammingError:
+        print("\n[Invalid credentials.]\n")
+        continue
+
+    except mysql.connector.errors.InterfaceError:
+        print("\n[Invalid address.]\n")
+        continue
+        
 
 username = database.user
 host = database._host
 
-print(f'Connected to {host} as {username}.\n(Use CTRL-C to exit.)\n')
+system("clear")
+print(f'Connected to {host} as {username}.\n')
 
 dbcursor = database.cursor()
 
 while True:
     try:
         query = input("["+username+"@"+host+"]> ")
+        if query.lower() in commands:
+            commands[query]()
+            continue
         dbcursor.execute(query)
         result = dbcursor.fetchall()
         print("\n====================================")
-        for x in dbcursor:
-            print(f'{x}')
         for x in result:
             print(f'{x}')
         print("====================================\n")
         continue
     except KeyboardInterrupt:
-        print(f'\n[Disconnecting from {host}...]\n')
-        input("Press enter to exit...")
-        exit(0)
+        exit()
     except mysql.connector.Error as error:
         if str(error) == "No result set to fetch from.":
             continue
